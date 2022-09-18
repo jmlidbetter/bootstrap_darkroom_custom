f4a55a2206773346531c3e8491e67e6b48fee943
Joseph Lidbetter <53430310+jmlidbetter@users.noreply.github.com>
2022-09-18 18:46:40 +0100

    Several tweaks to base theme:     * Force footer to sit at the bottom of the page     * Remove contact email address from footer     * Centre mainpage categories

---
+++ themeconf.inc.php
@@ -0,0 +1,13 @@
+<?php
+ 
+/*
+Theme Name: Bootstrap darkroom custom
+Version: 1
+Description: Small tweaks on top of bootstrap_darkroom
+Author: J M Lidbetter
+*/
+ 
+$themeconf = array(
+  'parent' => 'bootstrap_darkroom',
+);
+?>
